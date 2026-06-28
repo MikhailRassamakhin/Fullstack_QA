28297912cfb2f1916826c99beb01175cc8abd100
MikhailRassamakhin <weryqwer52@gmail.com>
2026-06-28 19:51:59 +0300

    Added classes, objects and constructors

--- src/main/java/ru/mihon/App.java
+++ src/main/java/ru/mihon/App.java
@@ -5,11 +5,19 @@ package ru.mihon;
  *
  */
 public class App 
-{    public static void main( String[] args )
-    {
-        String x = "Hello, world!";
-        System.out.println(x);
+{    public static void main(String[] args) {
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-        System.out.println("LOH");
+        Rectangle r = new Rectangle(6, 2);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    }
+
+    public static double area(Square s) {
+        return s.l * s.l;
+    }
+
+    public static double area(Rectangle r) {
+        return r.a * r.b;
     }
 }
---
+++ src/main/java/ru/mihon/Square.java
@@ -0,0 +1,9 @@
+package ru.mihon;
+
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+}
